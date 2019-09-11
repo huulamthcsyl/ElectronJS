2971b28d54b184b54b5929101b2986508525288d
huulamthcsyl <huulamthcsyl@gmail.com>
2019-09-11 17:46:51 +0700

    Commit

--- hello-world/main.js
+++ hello-world/main.js
@@ -1,1 +1,29 @@
-console.log("Hello World")
+const electron = require("electron");
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+const path = require("path");
+const url = require("url");
+
+let win;
+
+function createWindow(){
+    win = new BrowserWindow();
+    win.loadFile('index.html');
+    win.on('closed', () => {
+        win = null
+    })
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+    if(process.platform != 'darwin'){
+        app.quit()
+    }
+})
+
+app.on('activate', () => {
+    if(win == null){
+        createWindow()
+    }
+})
